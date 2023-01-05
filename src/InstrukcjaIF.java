6282876d1c08815b8d36cb18ee4d90d4f78233c1
RaQ13 <lukasz46@op.pl>
2023-01-05 10:42:46 +0100

    instukcja if

---
+++ src/InstrukcjaIF.java
@@ -0,0 +1,13 @@
+public class InstrukcjaIF {
+
+    public static void main(String[] args) {
+        float dzielna = 4.0F;
+        float dzielnik = 2.0F;
+
+        if (dzielnik != 0) {
+            System.out.println("Wynik dzielenia: " + dzielna/dzielnik);
+        } else {
+            System.out.println("Podaj inną liczbę`");
+        }
+    }
+}
