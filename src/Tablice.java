65ab92350210a06c84682bbe6197ab10bef78c19
RaQ13 <lukasz46@op.pl>
2023-01-10 10:22:25 +0100

    tablice

---
+++ src/Tablice.java
@@ -0,0 +1,30 @@
+public class Tablice {
+
+    public static void main(String[] args) {
+
+        String[] imiona = new String[3];
+
+        imiona[0] = "Iza";
+        imiona[1] = "Basia";
+        imiona[2] = "Marlena";
+
+//        System.out.println(imiona[0]);
+//        System.out.println(imiona[1]);
+//        System.out.println(imiona[2]);
+
+        int[] lottoNumbers = new int[] {1,2,3,4,5,6};
+
+//        System.out.println(lottoNumbers[0]);
+//        System.out.println(lottoNumbers[1]);
+//        System.out.println(lottoNumbers[2]);
+//        System.out.println(lottoNumbers[3]);
+//        System.out.println(lottoNumbers[4]);
+//        System.out.println(lottoNumbers[5]);
+//
+//        System.out.println(lottoNumbers.length);
+
+        for (int i = 0; i < imiona.length; i++) {
+            System.out.println(lottoNumbers[i]);
+        }
+    }
+}
