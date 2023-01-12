d3cd6a6dee6f8f071118c1e89688416da1d38133
RaQ13 <lukasz46@op.pl>
2023-01-12 08:57:10 +0100

    metody cz.2

--- src/Metody.java
+++ src/Metody.java
@@ -19,4 +19,24 @@ public class Metody {
        // return "hello" nie może zwrócić strniga bo zadeklarowany jest typ int
         return result;
     }
+
+    public int pobierzWynikParam(int number) {
+        System.out.println("Number ma wartość: " + number);
+        System.out.println("Zaraz policzę wynik");
+        int result = 0;
+        for (int i = 0; i < number; i++) {
+            result = result +i;
+        }
+        System.out.println("Rezultat to: " + result);
+        // return "hello" nie może zwrócić strniga bo zadeklarowany jest typ int
+        return result;
+    }
+
+    public int add(int firstNumber, int secondNumber, String word) { //mogą być podane różne typy danych
+        System.out.println("Suma to: " + (firstNumber + secondNumber));
+        //w sout first i second muszą być razem w nawiasie
+        // w przeciwnym wypadku wynikiem będzie wypisanie liczb obok siebie zamiast dodania
+        System.out.println(word);
+        return firstNumber + secondNumber;
+    }
 }
--- src/MetodyTest.java
+++ src/MetodyTest.java
@@ -2,14 +2,24 @@ public class MetodyTest {
 
     public static void main(String[] args) {
         Metody metody = new Metody();
-        metody.policzWynik();
-        metody.policzWynik();
-        metody.policzWynik();
-        metody.policzWynik();
+//        metody.policzWynik();
+//        metody.policzWynik();
+//        metody.policzWynik();
+//        metody.policzWynik();
+//
+//        int result = metody.pobierzWynik();
+//        int result2 = result * 2;
+//        System.out.println("Rezultat przed mnożeniem to: " + result);
+//        System.out.println("Rezultat po mnożeniu to " + result2);
 
-        int result = metody.pobierzWynik();
-        int result2 = result * 2;
-        System.out.println("Rezultat przed mnożeniem to: " + result);
-        System.out.println("Rezultat po mnożeniu to " + result2);
+        metody.pobierzWynikParam(2);
+        metody.pobierzWynikParam(100);
+        metody.pobierzWynikParam(23);
+        metody.pobierzWynikParam(2303);
+
+        metody.add(2,3, "wynik");
+        System.out.println(metody.add(2,3, "wynik")); //moża wypisać to co zwraca metoda w return
+        int result = metody.add(8,10, "wynik"); // można przypisać do zmiennej
+        System.out.println(result);
     }
 }
