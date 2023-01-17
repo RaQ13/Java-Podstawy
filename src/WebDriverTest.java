2569694c6506f8f38b82fe97a860a85422a56481
RaQ13 <lukasz46@op.pl>
2023-01-17 11:24:00 +0100

    polimorfizm przyklad

--- src/WebDriverTest.java
+++ src/WebDriverTest.java
@@ -2,12 +2,33 @@ public class WebDriverTest {
 
     public static void main(String[] args) {
 
-        ChromeDriverZadanie8 chrome = new ChromeDriverZadanie8();
-        chrome.get();
-        chrome.findElementBy();
+//        ChromeDriverZadanie8 chrome = new ChromeDriverZadanie8();
+//        chrome.get();
+//        chrome.findElementBy();
+//
+//        FirefoxDriverZadanie8 firefox = new FirefoxDriverZadanie8();
+//        firefox.get();
+//        firefox.findElementBy();
 
-        FirefoxDriverZadanie8 firefox = new FirefoxDriverZadanie8();
-        firefox.get();
-        firefox.findElementBy();
+        // z użyciem polimorfizmu bez duplikacji kodu
+
+        Zadanie8WebDriver driver = getDriver("firefox");
+        driver.get();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+    }
+
+    private static Zadanie8WebDriver getDriver(String name) {
+        if (name.equals("chrome")) {
+            return new ChromeDriverZadanie8(); // zwracanie obiektu
+        }
+        else if (name.equals("firefox")) {
+            return new FirefoxDriverZadanie8();
+        }
+        return null; // domyślna wartość
     }
 }
